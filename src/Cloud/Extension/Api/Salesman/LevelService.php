0d1a31106cfc42696fb0de2b564ed60600d333d2
app <app@aembd-pre-baymax0>
2019-07-18 16:23:42 +0800

    commit a version: 1.1.0.14, snapshot: false

--- src/Cloud/Extension/Api/Salesman/LevelService.php
+++ src/Cloud/Extension/Api/Salesman/LevelService.php
@@ -4,13 +4,9 @@ namespace Com\Youzan\Cloud\Extension\Api\Salesman;
 
 use Com\Youzan\Cloud\Extension\Param\Salesman\ExtLevelRequestDTO;
 use Com\Youzan\Cloud\Extension\Param\Salesman\AllLevelRulesResponseDTOOutParam;
-use Com\Youzan\Cloud\Extension\Param\Salesman\ExtLevelChangeRequestDTO;
-use Com\Youzan\Cloud\Extension\Param\Salesman\ExtLevelChangeResponseDTOOutParam;
 
 interface LevelService {
 
     public function getCustomLevelRules(ExtLevelRequestDTO $extLevelRequest) : AllLevelRulesResponseDTOOutParam;
-
-    public function changeLevel(ExtLevelChangeRequestDTO $extLevelChangeRequest) : ExtLevelChangeResponseDTOOutParam;
 
 }
