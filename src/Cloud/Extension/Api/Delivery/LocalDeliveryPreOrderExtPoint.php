0719189aa4d891438f3de693a53e3b4302b58f0d
app <app@aembd-prod-baymax0>
2019-10-08 11:02:56 +0800

    commit a version: 1.0.7.28, snapshot: true

--- src/Cloud/Extension/Api/Delivery/LocalDeliveryPreOrderExtPoint.php
+++ src/Cloud/Extension/Api/Delivery/LocalDeliveryPreOrderExtPoint.php
@@ -2,11 +2,11 @@
 
 namespace Com\Youzan\Cloud\Extension\Api\Delivery;
 
-use Com\Youzan\Cloud\Extension\Param\Delivery\LocalDeliveryFeeResponseDTO;
+use Com\Youzan\Cloud\Extension\Param\Delivery\LocalDeliveryOrderDTO;
 use Com\Youzan\Cloud\Extension\Param\Delivery\LocalDeliveryPreOrderResponseDTOOutParam;
 
 interface LocalDeliveryPreOrderExtPoint {
 
-    public function handle(LocalDeliveryFeeResponseDTO $localDeliveryFeeDTO) : LocalDeliveryPreOrderResponseDTOOutParam;
+    public function handle(LocalDeliveryOrderDTO $localDeliveryFeeDTO) : LocalDeliveryPreOrderResponseDTOOutParam;
 
 }
