33767bf80db096b8840b11dba5b5d8860de909ca
app <app@aembd-prod-baymax0>
2019-12-27 16:36:53 +0800

    commit a version: 1.1.1.17, snapshot: false

--- src/Cloud/Extension/Api/Salesman/AccountService.php
+++ src/Cloud/Extension/Api/Salesman/AccountService.php
@@ -4,13 +4,9 @@ namespace Com\Youzan\Cloud\Extension\Api\Salesman;
 
 use Com\Youzan\Cloud\Extension\Param\Salesman\ExtAccountVerifyRequestDTO;
 use Com\Youzan\Cloud\Extension\Param\Salesman\ExtAccountVerifyResponseDTOOutParam;
-use Com\Youzan\Cloud\Extension\Param\Salesman\ExtAccountRequestDTO;
-use Com\Youzan\Cloud\Extension\Param\Salesman\ExtAccountResponseDTOOutParam;
 
 interface AccountService {
 
     public function verify(ExtAccountVerifyRequestDTO $extAccountVerifyRequest) : ExtAccountVerifyResponseDTOOutParam;
-
-    public function becomeSalesman(ExtAccountRequestDTO $extAccountRequest) : ExtAccountResponseDTOOutParam;
 
 }
--- src/Cloud/Extension/Param/Salesman/ExtCpsCalculateRequest.php
+++ src/Cloud/Extension/Param/Salesman/ExtCpsCalculateRequest.php
@@ -8,7 +8,7 @@ use Com\Youzan\Cloud\Extension\Param\Salesman\ExtPromotionDTO;
 /**
  * 请求参数
  * @author Baymax
- * @create 2019-12-12 09:29:19.0
+ * @create Thu Dec 12 09:29:19 CST 2019
  */
 class ExtCpsCalculateRequest implements \JsonSerializable {
 
