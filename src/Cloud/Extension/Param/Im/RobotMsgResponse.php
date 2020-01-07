7a348fe57cbbb878f04d4afb2f7dc9bd995b8981
app <app@aembd-prod-baymax0>
2020-01-07 15:16:47 +0800

    commit a version: 1.1.1.20, snapshot: false

--- src/Cloud/Extension/Param/Im/RobotMsgResponse.php
+++ src/Cloud/Extension/Param/Im/RobotMsgResponse.php
@@ -12,13 +12,13 @@ namespace Com\Youzan\Cloud\Extension\Param\Im;
 class RobotMsgResponse implements \JsonSerializable {
 
     /**
-     * 机器人响应的消息内容，将会回复给用户
+     * 当消息类型为image时，响应content的内容使用图片链接，请将图片通过素材接口youzan.materials.storage.platform.img.upload上传，拿到有赞图片链接响应；当消息类型为faq时，响应content必须为标准json串，如以下示例{&quot;message&quot;:&quot;faq消息标题&quot;,&quot;faq_items&quot;:[{&quot;faq_id&quot;:0,&quot;keyword&quot;:&quot;1&quot;,&quot;title&quot;:&quot;申请换货&quot;},{&quot;faq_id&quot;:1,&quot;keyword&quot;:&quot;2&quot;,&quot;title&quot;:&quot;申请退款&quot;},{&quot;faq_id&quot;:2,&quot;keyword&quot;:&quot;3&quot;,&quot;title&quot;:&quot;其他咨询&quot;}],&quot;faq_type&quot;:&quot;robot_suggestion&quot;}
      * @var string
      */
     private $content;
 
     /**
-     * 消息类型，文本：text，图片：image，当消息类型为image时，响应content的内容使用图片链接，请将图片通过素材接口youzan.materials.storage.platform.img.upload上传，拿到有赞图片链接响应
+     * 消息类型，文本：text，图片：image，faq问答：faq
      * @var string
      */
     private $msgType;
