f4218030f5cea1ecb98cd8a10e1d6cd55caa3938
app <app@aembd-pre-baymax1>
2021-03-02 23:55:37 +0800

    commit a version: 1.1.3.44, snapshot: false

---
+++ src/Cloud/Extension/Api/Extpoint/MpAccountService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Com\Youzan\Cloud\Extension\Api\Extpoint;
+
+use Com\Youzan\Cloud\Extension\Param\Request\ExtGetMpAccountInfoDTO;
+use Com\Youzan\Cloud\Extension\Param\Response\ExtMpAccountInfoDTOOutParam;
+
+interface MpAccountService {
+
+    public function getMpAccountInfo(ExtGetMpAccountInfoDTO $request) : ExtMpAccountInfoDTOOutParam;
+
+}
---
+++ src/Cloud/Extension/Param/Response/ExtMpAccountInfoDTOOutParam.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Com\Youzan\Cloud\Extension\Param\Response;
+
+use Com\Youzan\Cloud\Extension\Param\Response\ExtMpAccountInfoDTO;
+
+/**
+ * 返回类
+ * @author Baymax
+ * @create 2021-02-24 17:14:34.0
+ */
+class ExtMpAccountInfoDTOOutParam implements \JsonSerializable {
+
+    /**
+     *
+     * @var bool
+     */
+    private $success;
+
+    /**
+     *
+     * @var string
+     */
+    private $code;
+
+    /**
+     *
+     * @var string
+     */
+    private $message;
+
+    /**
+     *
+     * @var ExtMpAccountInfoDTO
+     */
+    private $data;
+
+    /**
+     * @return bool
+     */
+    public function isSuccess(): bool
+    {
+        return $this->success;
+    }
+
+    /**
+     * @param bool $success
+     */
+    public function setSuccess(bool $success): void
+    {
+        $this->success = $success;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode(): string
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param string $code
+     */
+    public function setCode(string $code): void
+    {
+        $this->code = $code;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     */
+    public function setMessage(string $message): void
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @return ExtMpAccountInfoDTO
+     */
+    public function getData(): ?ExtMpAccountInfoDTO
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param ExtMpAccountInfoDTO $data
+     */
+    public function setData(?ExtMpAccountInfoDTO $data): void
+    {
+        $this->data = $data;
+    }
+
+    public function jsonSerialize() {
+        return get_object_vars($this);
+    }
+}
